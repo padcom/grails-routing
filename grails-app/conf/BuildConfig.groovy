ab4d6b4e791c63f8dd409f561c28c082a02fcd30
Maciej Hryniszak <maciej.hryniszak@lumesse.com>
2012-05-20 10:56:53 +0200

    Indentation cleanup

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,34 +1,34 @@
 grails.project.work.dir = "target"
 
 grails.project.dependency.resolution = {
-    inherits("global") {
-        excludes 'tomcat'
-        excludes 'hibernate'
-    }
+	inherits("global") {
+		excludes 'tomcat'
+		excludes 'hibernate'
+	}
 
-    log "warn"
+	log "warn"
 
-    repositories {
-        grailsPlugins()
-        grailsHome()
-        grailsCentral()
+	repositories {
+		grailsPlugins()
+		grailsHome()
+		grailsCentral()
 
-        mavenLocal()
-        mavenCentral()
-    }
+		mavenLocal()
+		mavenCentral()
+	}
 
-    dependencies {
-        compile('org.apache.camel:camel-core:2.9.0')
-        compile('org.apache.camel:camel-spring:2.9.0') {
-            excludes 'spring-aop', 'spring-beans', 'spring-core', 'spring-expression', 'spring-asm', 'spring-tx', 'spring-context'
-        }
-        compile('org.apache.camel:camel-groovy:2.9.0') {
-            excludes 'spring-context', 'spring-aop', 'spring-tx', 'groovy-all'
-        }
-        compile('org.apache.camel:camel-stream:2.9.0')
-    }
+	dependencies {
+		compile('org.apache.camel:camel-core:2.9.0')
+		compile('org.apache.camel:camel-spring:2.9.0') {
+			excludes 'spring-aop', 'spring-beans', 'spring-core', 'spring-expression', 'spring-asm', 'spring-tx', 'spring-context'
+		}
+		compile('org.apache.camel:camel-groovy:2.9.0') {
+			excludes 'spring-context', 'spring-aop', 'spring-tx', 'groovy-all'
+		}
+		compile('org.apache.camel:camel-stream:2.9.0')
+	}
 
-    plugins {
-        build(":release:2.0.0", ':rest-client-builder:1.0.2') { export = false }
-    }
+	plugins {
+		build(":release:2.0.0", ':rest-client-builder:1.0.2') { export = false }
+	}
 }
