2c27696106f2fe84be4485a029a5b2e524276514
Arsen A. Gutsal <gutsal.arsen@softsky.com.ua>
2013-01-25 15:33:13 +0200

    upgraded to grails-2.1.1, removed :release:2.0.0, :rest-client-builder-1.0.2 dependencies

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -29,6 +29,6 @@ grails.project.dependency.resolution = {
 	}
 
 	plugins {
-		build(":release:2.0.0", ':rest-client-builder:1.0.2') { export = false }
+              //build(":release:2.0.0", ':rest-client-builder:1.0.2') { export = false }
 	}
 }
