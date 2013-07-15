2fc5402ba43fdeec24c88f1e3cbdd222318ce91e
Arsen A. Gutsal <gutsal.arsen@softsky.com.ua>
2013-07-15 11:46:42 +0300

    release plugin upgraded to 2.2.1

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -31,6 +31,8 @@ grails.project.dependency.resolution = {
 	}
 
 	plugins {
-		build(":release:2.2.0") { export = false }
+                build ':release:2.2.1', ':rest-client-builder:1.0.3', {
+                  export = false
+                }
 	}
 }
