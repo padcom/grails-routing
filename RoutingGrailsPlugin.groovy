ab75b038e592db6ae4d19a6642b189c8f3305810
Arsen A. Gutsal <gutsal.arsen@softsky.com.ua>
2013-07-15 16:26:41 +0300

    usage of CamelGroovyMethods class @deprecated since camel 2.11.0 is replaced with the right one

--- RoutingGrailsPlugin.groovy
+++ RoutingGrailsPlugin.groovy
@@ -2,7 +2,7 @@ import grails.util.*
 
 import org.apache.camel.spring.*
 import org.apache.camel.model.*
-import org.apache.camel.language.groovy.CamelGroovyMethods
+import org.apache.camel.groovy.extend.CamelGroovyMethods
 import org.springframework.beans.factory.config.MethodInvokingFactoryBean
 
 import org.grails.plugins.routing.RouteArtefactHandler
@@ -95,22 +95,22 @@ class RoutingGrailsPlugin {
 	private initializeRouteBuilderHelpers() {
 		ProcessorDefinition.metaClass.filter = { filter ->
 			if (filter instanceof Closure) {
-				filter = CamelGroovyMethods.toExpression(filter)
-			}
+                          filter = CamelGroovyMethods.filter(delegate, filter)
+			} else 
 			delegate.filter(filter);
 		}
 
 		ChoiceDefinition.metaClass.when = { filter ->
 			if (filter instanceof Closure) {
-				filter = CamelGroovyMethods.toExpression(filter)
-			}
+                          filter = CamelGroovyMethods.when(delegate, filter)
+			} else
 			delegate.when(filter);
 		}
 
 		ProcessorDefinition.metaClass.process = { filter ->
 			if (filter instanceof Closure) {
-				filter = new ClosureProcessor(filter)
-			}
+                          filter = CamelGroovyMethods.process(delegate, filter)
+			} else 
 			delegate.process(filter);
 		}
 	}
