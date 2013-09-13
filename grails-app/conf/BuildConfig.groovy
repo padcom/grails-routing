3004451774b1455e18d65947f581dff971119c54
Arsen A. Gutsal <gutsal.arsen@softsky.com.ua>
2013-09-13 21:37:01 +0300

    grails upgraded to 2.2.4, release plugin downgraded to 2.2.1 as 3.0.0 require grails 2.3.0

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -25,7 +25,7 @@ grails.project.dependency.resolution = {
 	}
 
 	plugins {
-		build ':release:3.0.0', ':rest-client-builder:1.0.3', {
+		build ':release:2.2.1', ':rest-client-builder:1.0.3', {
 			export = false
 		}
 	}
