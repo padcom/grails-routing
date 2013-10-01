3480ee891fb54c9938cb4f6557221aba79b46a0d
Arsen A. Gutsal <gutsal.arsen@softsky.com.ua>
2013-10-01 11:21:28 +0300

    camel version upgraded to 2.12.1, plugin version increased to 1.2.7

--- RoutingGrailsPlugin.groovy
+++ RoutingGrailsPlugin.groovy
@@ -5,7 +5,7 @@ import org.grails.plugins.routing.RouteArtefactHandler
 import org.springframework.beans.factory.config.MethodInvokingFactoryBean
 
 class RoutingGrailsPlugin {
-	def version          = '1.2.6'
+	def version          = '1.2.7'
 	def grailsVersion    = '2.0.0 > *'
 	def loadAfter        = ['controllers', 'services']
 	def artefacts        = [new RouteArtefactHandler()]
