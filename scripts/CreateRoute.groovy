1097fabba50c921fcbb38bbd0a82df3ba8f982f4
Tristan Lohman <gatorcse@gmail.com>
2014-01-08 13:16:07 -0500

    #33 Fill in createArtifact arguments
    Copied the relevant bits from #34.
    
    Developed on Grails 2.3.4

--- scripts/CreateRoute.groovy
+++ scripts/CreateRoute.groovy
@@ -1,11 +1,13 @@
 includeTargets << grailsScript("_GrailsInit")
+includeTargets << grailsScript("_GrailsCreateArtifacts")
 
 target(createRoute: "Creates a new Camel Route.") {
+    name = argsMap.params ? argsMap.params[0] : 'Example'
     typeName = "Route"
     artifactName = "Route"
     artifactPath = "grails-app/routes"
 
-    createArtifact()
+    createArtifact(type: typeName, path: artifactPath, name: name, suffix: 'Route')
 }
 
 setDefaultTarget(createRoute)
