baf6ab646598206b58c78b8a166168032d307864
Arsen A. Gutsal <gutsal.arsen@softsky.com.ua>
2014-03-07 19:10:28 +0200

    v1.3.0 released, now require grails v.2.3.5 to run

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,6 +1,28 @@
 grails.project.work.dir = 'target'
 
-def camelVersion = '2.12.1'
+def camelVersion = '2.12.3'
+
+grails.project.fork = [
+
+        // configure settings for the test-app JVM, uses the daemon by default
+
+        test: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, daemon: true],
+
+// configure settings for the run-app JVM
+
+        run: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve: false],
+
+// configure settings for the run-war JVM
+
+        war: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve: false],
+
+// configure settings for the Console UI JVM
+
+        console: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256]
+
+]
+
+grails.project.dependency.resolver = "maven" // or ivy
 
 grails.project.dependency.resolution = {
 
@@ -27,8 +49,8 @@ grails.project.dependency.resolution = {
 	}
 
 	plugins {
-		build ':release:2.2.1', ':rest-client-builder:1.0.3', {
-			export = false
+		build ':release:3.0.1', ':rest-client-builder:2.0.1', {
+                  //			export = false
 		}
 	}
 }
